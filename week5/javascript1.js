621ca363e84e0d5f7cb26d4f153fe9017e6c4943
alkantedam <alkantedam@gmail.com>
2017-10-04 21:16:05 +0600

    Fifth commit

---
+++ week5/javascript1.js
@@ -0,0 +1,24 @@
+function onCLick() {
+	const first = document.querySelector("#first");
+	const second = document.querySelector("#second");
+	const third = document.querySelector("#third");
+	const fourth = document.querySelector("#fourth");
+	const fifth = document.querySelector("#fifth");
+	const sixth = document.querySelector("#sixth");
+	const seventh = document.querySelector("#seventh");
+	const eighth = document.querySelector("#eighth");
+	const nineth = document.querySelector("#nineth");
+
+	const result = document.querySelector("#result");
+
+
+
+	let det = parseInt(first.value)*(parseInt(fifth.value)*parseInt(nineth.value) - parseInt(sixth.value)*parseInt(eighth.value)) - 
+				parseInt(second.value)*(parseInt(fourth.value)*parseInt(nineth.value) - parseInt(sixth.value)*parseInt(seventh.value)) +
+				parseInt(third.value)*(parseInt(fourth.value)*parseInt(eighth.value) - parseInt(fifth.value)*parseInt(seventh.value));
+
+	result.value = det;	
+}
+
+const button = document.querySelector('button');
+button.addEventListener('click', onCLick);
